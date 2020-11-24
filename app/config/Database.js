3114c60a78a73079e4a022b7f342532c66c0c0a2
Hector Blandon <hlblandone@uqvirtual.edu.co>
2020-11-24 00:11:00 -0500

    modificacion a github

--- app/config/Database.js
+++ app/config/Database.js
@@ -1,15 +1,15 @@
 const MAX_CONNECTION_POOLSIZE = 5;
 
 const {
-    DB_NAME = 'automatik',
-        DB_USER = 'postgres',
-        DB_PASS = '123456',
-        DB_HOST = 'localhost',
+    // DB_NAME = 'automatik',
+    //     DB_USER = 'postgres',
+    //     DB_PASS = '123456',
+    //     DB_HOST = 'localhost',
     //     DB_PORT = 5432,
-    // DB_NAME = 'd3p941o1hdj9ve',
-    // DB_USER = 'ubfqjhmiktpikb',
-    // DB_PASS = '8728593844af58e2e0361a7d7dc5fd16edd97248bdbb736beaa5657efee1f0c7',
-    // DB_HOST = 'ec2-3-95-87-221.compute-1.amazonaws.com',
+    DB_NAME = 'd3p941o1hdj9ve',
+    DB_USER = 'ubfqjhmiktpikb',
+    DB_PASS = '8728593844af58e2e0361a7d7dc5fd16edd97248bdbb736beaa5657efee1f0c7',
+    DB_HOST = 'ec2-3-95-87-221.compute-1.amazonaws.com',
     DB_PORT = 5432,
 } = process.env;
 
