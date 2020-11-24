51a633c70e6990f941d0bfda77ebe3f6a9ad5832
Hector Blandon <hlblandone@uqvirtual.edu.co>
2020-11-23 23:56:48 -0500

    se comenta url
    Se comenta url de produccion y se deja url local para pruebas

--- app/config/AppConfig.js
+++ app/config/AppConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
     PREFIX: '/api/backend',
     MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://backend-automatik.herokuapp.com/',
+    //  MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://localhost:3000',
 };
