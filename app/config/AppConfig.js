fbebca7d70f562569061f28c700fc65f5c657dda
hector-blandon <65681461+hector-blandon@users.noreply.github.com>
2020-11-25 01:02:46 -0500

    modificación para conexión
    se cambia url

--- app/config/AppConfig.js
+++ app/config/AppConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     PREFIX: '/api/backend',
-   // MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://backend-automatik.herokuapp.com/',
-   MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://localhost:3000',
+   MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://backend-automatik.herokuapp.com/',
+   // MICROSERVICE_URL: process.env.MICROSERVICE_URL || 'http://localhost:3000',
 };
